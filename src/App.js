c99211f8690413f7944f08d44242237e7bc69692
MNewman <megan.m.boggs@gmail.com>
2022-06-28 01:06:15 -0700

    forgot that containers are divs...
    🫠

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ import Dictionary from "./Dictionary.js";
 export default function App() {
   return (
     <div className="App">
-      <container>
+      <div className="container">
+        <header className="App-header"></header>
         <main>
           <Dictionary />
         </main>
-      </container>
-      <footer>
-        This app was coded by <a href="https://mnewman.io">MNewman</a> and is{" "}
-        <a href="https://github.com/NewmanSaurusRex/react-dictionary-app">
-          open sourced on GitHub
-        </a>{" "}
-        and hosted on{" "}
-        <a href="https://dazzling-lily-10fca4.netlify.app/">Netlify</a>
-      </footer>
+
+        <footer>
+          This app was coded by <a href="https://mnewman.io">MNewman</a> and is{" "}
+          <a href="https://github.com/NewmanSaurusRex/react-dictionary-app">
+            open sourced on GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a href="https://dazzling-lily-10fca4.netlify.app/">Netlify</a>
+        </footer>
+      </div>
     </div>
   );
 }
