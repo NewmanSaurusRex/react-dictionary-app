861ce5ddbfe43e17ff8917bd009d970052921ab7
MNewman <megan.m.boggs@gmail.com>
2022-06-29 01:48:57 -0700

    added axios and api call w/console.log to verify :)
    added documentation note to refer back to api info from api developer

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,20 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
   function search(event) {
     event.preventDefault();
-    alert(`You are searching for ${keyword}...`);
   }
+
+  //note: https://dictionaryapi.dev/ documentation for reference//
+  let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+  axios.get(apiUrl).then(handleResponse);
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
