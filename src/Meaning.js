12c9ff07e0d683acdb0935aa33e249907fb5ebdc
MNewman <megan.m.boggs@gmail.com>
2022-06-30 23:52:02 -0700

    updated jsx to only search on submit

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,20 +1,22 @@
 import React from "react";
 
 export default function Meaning(props) {
-  return (
-    <div className="Meaning">
-      <h3>{props.meaning.partOfSpeech}</h3>
-      {props.meaning.definitions.map(function (definition, index) {
-        return (
-          <div key={index}>
-            <p>
-              {definition.definition}
+  if (props.meaning) {
+    return (
+      <div className="Meaning">
+        <h3>{props.meaning.partOfSpeech}</h3>
+        {props.meaning.definitions.map(function (definition, index) {
+          return (
+            <div key={index}>
+              <p>{definition.definition}</p>
               <br />
               <em>{definition.example}</em>
-            </p>
-          </div>
-        );
-      })}
-    </div>
-  );
+            </div>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
