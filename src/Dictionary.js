f14eb4663d88ea03610382574fea40e494652133
MNewman <megan.m.boggs@gmail.com>
2022-07-04 19:57:10 -0700

    added new api call to gather photos
    storing photos inside state and sending them to the component, looping through photos and requesting 9 photos for UI. if you click each photo user is directed to og img from api call

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,32 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsReponse(response) {
+    setResults(response.data.photos);
   }
 
   function search() {
     //note:https://dictionaryapi.dev/ documentation for reference//
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let pexelsKey = "563492ad6f91700001000001829a48387d144249afaf81d5e2991b1e";
+    let pexelsUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer${pexelsKey}` };
+    axios.get(pexelsUrl, { headers: headers }).then(handlePexelsReponse);
   }
 
   function handleSubmit(event) {
@@ -51,6 +62,7 @@ if (loaded) {
         <div className="hint">Suggested Words: Pizza, Cake, Planet, Epic, </div>
       </section>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 } else {
